fd6540017a14ad26d139aff2610dd17be2d3e15a
unknown <16gandhi.hemani@gmail.com>
2023-06-29 17:48:15 +0530

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -51,7 +51,7 @@ $sendLocationButton.addEventListener('click', () => {
            search.value = latitude+' ,'+longitude
            messageOne.textContent = 'Loading...'
            messageTwo.textContent = ''
-           fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/'+longitude+','+latitude+'.json?access_token=pk.eyJ1IjoiZGcwMDciLCJhIjoiY2xpZWVxaTlvMHphazNmcW82YnZpOHhjdSJ9.pYgO1uWy2WnWfM-5I6Os_A').then((response) => {    
+           fetch('https://api.mapbox.com/geocoding/v5/mapbox.places/'+longitude+','+latitude+'.json?access_token='+ process.env.MAPBOX_API_KEY).then((response) => {    
            response.json().then((data) => {
                 if(data.error){
                   messageOne.textContent = data.error
@@ -60,21 +60,23 @@ $sendLocationButton.addEventListener('click', () => {
                     const locality  = data.features[0].place_name.split(',')
                     const len = locality.length - 3
                     const location = locality.slice(len, locality.length);
-                    console.log(locality)
                     fetch('/weather?address='+ location).then((response)=> {
                          response.json().then((data) => {
                          if(data.error){
                             messageOne.textContent = data.error
                          }
                          else{
-                            messageOne.textContent = data.location
-                            const weather = ['cloudy' , 'sunny' , 'rain' , 'snowy' , 'stormy']
+                            messageOne.textContent = location
                             const forecastData = data.forecast;
-                            var i, len = weather.length;
+                            var i;
                             for (i=0; i<len; ++i) {
-                                if(forecastData.includes(weather[i])){
-                                    document.getElementById(weather[i]).style.display = 'inline';
-                                 }
+                            if(forecastData.toLowerCase().includes(weather[i])){
+                                const gif = document.getElementById(weather[i]);
+                                gif.style.display='inline'
+                                setTimeout(() => {
+                                    gif.style.display = 'none';
+                                  }, 30000);              
+                              }
                             }
                             messageTwo.textContent = data.forecast
                             }
